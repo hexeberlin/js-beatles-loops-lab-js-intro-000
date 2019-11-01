59fddd90582713193622f78465972be5d658cd94
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let result = []
+  for(let i=0; i++; i<musicians.length){
+    result.push(musicians[i]+" plays "+instruments[i])
+  }
+  return result
+}
+
+function johnLennonFacts(facts){
+  let result = []
+  do {
+    
+  }
+}
